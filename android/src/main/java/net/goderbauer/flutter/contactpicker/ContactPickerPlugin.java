db28e10712809802253caa3c46d40d8b4ecae9e7
Michael Goderbauer <michael@goderbauer.net>
2017-09-18 21:57:55 -0700

    add LICENSE header

--- android/src/main/java/net/goderbauer/flutter/contactpicker/ContactPickerPlugin.java
+++ android/src/main/java/net/goderbauer/flutter/contactpicker/ContactPickerPlugin.java
@@ -1,3 +1,7 @@
+// Copyright 2017 Michael Goderbauer. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package net.goderbauer.flutter.contactpicker;
 
 import android.app.Activity;
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,7 @@
+// Copyright 2017 Michael Goderbauer. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:contact_picker/contact_picker.dart';
 
